983ca49d14e8b0642d2f97d7dd623fc79df80932
Eric de Vulpillières <ericvulpi@gmail.com>
2017-01-28 01:25:34 +0100

    Initial Commit

---
+++ MusicBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicBar
+//
+//  Created by Eric de Vulpillières on 28/01/2017.
+//  Copyright © 2017 Eric de Vulpillières. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MusicBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MusicBar
+//
+//  Created by Eric de Vulpillières on 28/01/2017.
+//  Copyright © 2017 Eric de Vulpillières. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
